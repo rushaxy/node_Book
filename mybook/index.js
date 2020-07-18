277f2de9381d09ec044b45ede5883e1af7e0bd63
rushaxy <1556658267@qq.com>
2020-07-19 00:15:24 +0800

    添加以debug方式启动

--- mybook/index.js
+++ mybook/index.js
@@ -8,6 +8,18 @@ const template = require('art-template');
 const bodyParser = require('body-parser');
 const app = express();
 
+
+
+// error ruturn to client  handle
+var httpError = require('http-errors');
+//cookie configuration
+var cookieParser = require('cookie-parser');
+//  日志打印
+var logger = require('morgan');
+
+
+
+
 // 启动静态资源服务
 app.use('/www',express.static('public'));
 
@@ -25,13 +37,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // 处理json格式的参数
 app.use(bodyParser.json());
 
+
+app.use(cookieParser());
+app.use(logger('dev'));
+
+
+
 // 启动服务器功能
 // 配置路由
-app.use(router);
-// 监听端口
-app.listen(3000,()=>{
-    console.log('running...');
+// app.use(router);
+app.use('/', router);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(httpError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
+
+// 监听端口
+// app.listen(3000,()=>{
+    // console.log('running...');
+// });
 
 
 
